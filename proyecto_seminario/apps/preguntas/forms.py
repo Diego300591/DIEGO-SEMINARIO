16ae6e4e018a609a59fcddde7a289bae02700c03
Diego <diegocastanares3005@gmail.com>
2014-10-27 23:47:05 -0400

    con formulario partidas incluido

--- proyecto_seminario/apps/preguntas/forms.py
+++ proyecto_seminario/apps/preguntas/forms.py
@@ -3,6 +3,8 @@ from django.forms import ModelForm
 from .models import *
 from django.forms.extras.widgets import *
 tipos=(('public','Publico'),('private','Privado'))
+cant_preguntas=(('0','10'),('1','20'),('2','30'),('3','40'),('4','50'))
+tiempo=(('0','10 segundos'),('1','15 segundos'),('2','20 segundos'),('3','25 segundos'),('4','30 segundos'),('5','35 segundos'),('6','40 segundos'),('7','45 segundos'),('8','50 segundos'),('9','55 segundos'),('10','60 segundos'))
 categoria=categorias.objects.all()
 class categoriaForm(forms.ModelForm):
 	class Meta:
@@ -13,8 +15,8 @@ class preguntaForm(ModelForm):
 		model=mpregunta
 		exclude=["usuario"]
 class partidaForm(ModelForm):
-	tipo_partida=forms.CharField(widget=forms.RadioSelect(tipo=tipos))
-	categorias_sel=forms.CharField(widget=forms.CheckboxSelectMultiple(categorias=categoria))
+	tipo_partida=forms.ChoiceField(widget=forms.RadioSelect,choices=tipos)
+	categorias_sel=forms.ModelMultipleChoiceField(queryset=categorias.objects.all(),widget=forms.CheckboxSelectMultiple())
 	class Meta:
 		model=partida
 		exclude=["usuario"]
