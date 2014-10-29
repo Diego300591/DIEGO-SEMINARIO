e4c7b7012d34ed2bef73d11925f9a74af3347258
Diego <diegocastanares3005@gmail.com>
2014-10-29 11:15:05 -0400

    pequeña modificacion

--- proyecto_seminario/apps/juego/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-# Create your models here.
-class Usuario(models.Model):
-	nick=models.CharField(max_length=200, unique=True)
-	email=models.EmailField()
-	password=models.CharField(max_length=200, null=True, blank=True)
-	fecha=models.DateField(auto_now=True)
-	def __unicode__(self):
-		return self.nick
