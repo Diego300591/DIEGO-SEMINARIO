ce7db31f65e6615acb4cc1556163caa5ba94001c
diego <diegocastanares3005@gmail.com>
2014-12-11 04:53:38 -0400

    ajustes finales django

--- proyecto_seminario/settings.py
+++ proyecto_seminario/settings.py
@@ -69,8 +69,11 @@ WSGI_APPLICATION = 'proyecto_seminario.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'trivia',
+        'USER': 'root',
+        'PASSWORD':'',
+        'HOST':'127.0.0.1',
     }
 }
 
